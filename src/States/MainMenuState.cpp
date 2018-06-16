31a80688d86b2d437a86f1dc6ee6e1e5dd293555
Your Name <you@example.com>
2018-06-16 08:04:33 +0200

    new features

--- src/States/MainMenuState.cpp
+++ src/States/MainMenuState.cpp
@@ -14,7 +14,9 @@
 #include "../InputHandler.h"
 #include "PlayState.h"
 #include <iostream>
-
+#include <chrono>
+#include <thread>
+#include "../Game.h"
 #include "MainMenuState.h"
 using namespace std;
 const std::string MainMenuState::s_menuID = "MENU";
@@ -24,6 +26,7 @@ void MainMenuState::update()
 	if(menuSelect==1&&changed==true)
 	{
 		TheSoundManager::Instance()->playMusic("Start",1);
+
 		changed=false;
 	}
 	else if (menuSelect==2&&changed==true)
@@ -39,20 +42,37 @@ void MainMenuState::update()
 	}
 	if(TheInputHandler::Instance()->isPressed(SDLK_UP))
 	{
-		menuSelect--;
-		cout<<menuSelect<<endl;
-		changed=true;
+		if(menuSelect==1)
+		{
+			TheSoundManager::Instance()->playMusic("Boing",1);
+		}
+		else
+		{
+			menuSelect--;
+			cout<<menuSelect<<endl;
+			changed=true;
+		}
 	}
 	if(TheInputHandler::Instance()->isPressed(SDLK_DOWN))
 	{
+
+		if(menuSelect==3)
+				{
+					TheSoundManager::Instance()->playMusic("Boing",1);
+				}
+				else
+				{
 		menuSelect++;
 		cout<<menuSelect<<endl;
 		changed=true;
+				}
 	}
 	if(TheInputHandler::Instance()->isPressed(SDLK_SPACE)||TheInputHandler::Instance()->isPressed(SDLK_RETURN))
 	{
 			cout<<"menu item " << menuSelect<< " is selected"<<endl;
-			if(menuSelect)
+			if(menuSelect==1)
+				TheGame::Instance()->getStateMachine()->changeState(new PlayState());
+			if(menuSelect==3)
 				exit(0);
 	}
 
@@ -67,18 +87,28 @@ void MainMenuState::render()
 }
 bool MainMenuState::onEnter()
 {
-	string pathSFX="assets/Lydfiler//Start et nyt spil.mp3";
+	string pathSFX="assets/Lydfiler/menu/Start et nyt spil.mp3";
 	TheSoundManager::Instance()->load(pathSFX,"Start",SOUND_MUSIC);
 
-	pathSFX="assets/Lydfiler/Afslut.mp3";
+	pathSFX="assets/Lydfiler/menu/Afslut.mp3";
 	TheSoundManager::Instance()->load(pathSFX,"Afslut",SOUND_MUSIC);
-	pathSFX="assets/Lydfiler/Indstillinger.mp3";
+	pathSFX="assets/Lydfiler/menu/Indstillinger.mp3";
 	TheSoundManager::Instance()->load(pathSFX,"Indstillinger",SOUND_MUSIC);
 
+
+	pathSFX="assets/Lydfiler/menu/Boing 2.mp3";
+	TheSoundManager::Instance()->load(pathSFX,"Boing",SOUND_MUSIC);
+
+	pathSFX="assets/Lydfiler/menu/Velkommen.mp3";
+	TheSoundManager::Instance()->load(pathSFX,"Velkommen",SOUND_MUSIC);
+	TheSoundManager::Instance()->playMusic("Velkommen",1);
+	using namespace std::this_thread; // sleep_for, sleep_until
+	using namespace std::chrono;
+	sleep_for(nanoseconds(1));
+	sleep_until(system_clock::now() + seconds(4));
 	std::cout << "entering MenuState\n";
 	return true;
 
-	return true;
 
 
 }
--- src/States/PlayState.cpp
+++ src/States/PlayState.cpp
@@ -17,13 +17,25 @@
 #include "../Game.h"
 #include "PauseState.h"
 #include "GameOverState.h"
-
+#include "../SoundManager.h"
 const std::string PlayState::s_playID = "PLAY";
+int playersTurn;
 void PlayState::update()
 {
-
-
-
+	if(TheInputHandler::Instance()->isPressed(SDLK_SPACE)||TheInputHandler::Instance()->isPressed(SDLK_RETURN))
+	{
+	if(playersTurn==1)
+	{
+		TheSoundManager::Instance()->playMusic("spiller 2s tur",1);
+		playersTurn=2;
+	}
+	else if(playersTurn==2)
+		{
+		TheSoundManager::Instance()->playMusic("spiller 1s tur",1);
+		playersTurn=1;
+		}
+	}
+	std::cout << "on update"<< playersTurn<<"\n";
 
 }
 void PlayState::render()
@@ -34,7 +46,16 @@ void PlayState::render()
 }
 bool PlayState::onEnter()
 {
+	playersTurn=1;
+	string pathSFX="assets/Lydfiler/spil/Brik.mp3";
+	TheSoundManager::Instance()->load(pathSFX,"Brik",SOUND_MUSIC);
+	pathSFX="assets/Lydfiler/spil/spiller 1s tur.mp3";
+	TheSoundManager::Instance()->load(pathSFX,"spiller 1s tur",SOUND_MUSIC);
 
+	pathSFX="assets/Lydfiler/spil/spiller 2s tur.mp3";
+		TheSoundManager::Instance()->load(pathSFX,"spiller 2s tur",SOUND_MUSIC);
+		TheSoundManager::Instance()->playMusic("spiller 1s tur",1);
+	std::cout << "on enter PlayState\n";
 	return true;
 }
 bool PlayState::onExit()
